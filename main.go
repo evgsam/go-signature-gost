8b7f92e741495a87ae26c82d0218fd6d64bb2a3e
evgsam <evgsamarin@gmail.com>
2026-05-24 11:17:15 +0300

    функция проверки подписи

--- main.go
+++ main.go
@@ -53,6 +53,45 @@ func (curve *CurveParams) Sign(d, e *big.Int) (r, s *big.Int, err error) { // П
 	}
 }
 
+func (curve *CurveParams) Verify(H *Point, msg []byte, r, s *big.Int) bool { // Проверка подписи
+	Q := curve.Q
+	// Проверяем, что r и s в правильном диапазоне (0 < r < Q и 0 < s < Q)
+	if r.Sign() <= 0 || r.Cmp(Q) >= 0 {
+		return false
+	}
+	if s.Sign() <= 0 || s.Cmp(Q) >= 0 {
+		return false
+	}
+	// Вычисляем e = hash(msg) mod Q
+	e := curve.hashToNumber(msg)
+	// Вычисляем v = e^(-1) mod Q
+	v := new(big.Int).ModInverse(e, Q)
+	if v == nil {
+		return false
+	}
+	// Вычисляем z1 = s * v mod Q и z2 = -r * v mod Q
+	z1 := new(big.Int).Mul(s, v)
+	z1.Mod(z1, Q)
+	z2 := new(big.Int).Mul(r, v)
+	z2.Neg(z2)
+	z2.Mod(z2, Q)
+	// G = (GX, GY) - базовая точка
+	G := &Point{X: curve.GX, Y: curve.GY, Inf: false}
+
+	//C' = z1 * G + z2 * H
+	p1 := curve.ScalarMult(z1, G)
+	p2 := curve.ScalarMult(z2, H)
+	Cp := curve.AddPoints(p1, p2)
+
+	// Если C' – точка на бесконечности, подпись недействительна
+	if Cp.Inf {
+		return false
+	}
+	// r' = C'.X mod Q
+	rPrime := new(big.Int).Mod(Cp.X, Q)
+	return rPrime.Cmp(r) == 0
+}
+
 func (curve *CurveParams) GenerateKey() (d *big.Int, H *Point, err error) {
 	Q := curve.Q
 	if Q == nil || Q.Sign() <= 0 {
